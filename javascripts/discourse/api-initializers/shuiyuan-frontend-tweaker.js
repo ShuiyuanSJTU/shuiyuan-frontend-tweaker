8517807d2163ca7d022793f0798af51c8450bc6a
pangbo13 <51732678+pangbo13@users.noreply.github.com>
2024-03-28 13:22:09 +0800

    fix chat image thumbnail

--- javascripts/discourse/api-initializers/shuiyuan-frontend-tweaker.js
+++ javascripts/discourse/api-initializers/shuiyuan-frontend-tweaker.js
@@ -6,4 +6,22 @@ export default apiInitializer("0.11.1", api => {
   if(window.navigator.userAgent.includes("TaskCenterApp")){
     document.querySelector("html").classList.add("jwb");
   }
+
+  const siteSettings = api._lookupContainer("service:site-settings");
+  if (siteSettings.secure_uploads && settings.fix_chat_image_thumbnail) {
+    api.modifyClass("component:chat-upload", {
+      get imageUrl() {
+        if (this.args.upload.thumbnail?.url) {
+          const url = new URL(this.args.upload.thumbnail?.url, location.href);
+          if (url.host !== location.host) {
+            url.host = location.host;
+            url.pathname = "/secure-uploads" + url.pathname;
+          }
+          return url.href;
+        } else {
+          return this.args.upload.url;
+        }
+      }
+    });
+  }
 });
